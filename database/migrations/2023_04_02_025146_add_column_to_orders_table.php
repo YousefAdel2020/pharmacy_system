e6454fcd311883e1d5b0f3a4a5c2f18449cb0a64
Omar Walid <omo_1712@yahoo.com>
2023-04-02 15:13:13 +0200

    fix migrations

--- database/migrations/2023_04_02_025146_add_column_to_orders_table.php
+++ database/migrations/2023_04_02_025146_add_column_to_orders_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::table('orders', function (Blueprint $table) {
             $table->unsignedBigInteger('ordered_by_id');
             $table->unsignedBigInteger('doctor_id');
+            $table->unsignedBigInteger('pharmacy_id')->nullable();
+            
+            
+            $table->foreign('pharmacy_id')->references('id')->on('pharmacies');
             $table->foreign('ordered_by_id')->references('id')->on('users');
             $table->foreign('doctor_id')->references('id')->on('doctors')->onDelete('cascade');
         });
