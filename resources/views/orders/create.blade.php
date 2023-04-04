f8df646e733c1f859b57b7e9d8a53b6fb632281c
Yousef Adel <yousefjooo2018@gmail.com>
2023-04-04 05:02:12 +0200

    add some feature at order controller

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -86,7 +86,7 @@
 
                   <div class="form-group">
                         <label for="PharmacyName">Pharmacy Name</label>
-                        <select name="PharmacyName" class="form-control select2 select2-hidden-accessible" style="width: 100%;" data-select2-id="1" tabindex="-1" aria-hidden="true">
+                        <select name="PharmacyName" class="form-control " style="width: 100%;" data-select2-id="1" tabindex="-1" aria-hidden="true">
                           @foreach($pharmacy as $phar)
                           <option>{{$phar->name}}</option>
                           @endforeach
