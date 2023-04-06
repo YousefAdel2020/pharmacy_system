9d5294a162c6c79eed64143b18f678fa73141bff
Omar Walid <omo_1712@yahoo.com>
2023-04-06 07:02:23 +0200

    fix bugs

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
+
 class OrderController extends Controller
 {
     public function index()
@@ -18,22 +19,22 @@ class OrderController extends Controller
         $orders = Auth::user()->order ?? Order::all();
         return OrderResource::collection($orders);
     }
-     public function show(Order $order)
+    public function show(Order $order)
     {
 
         return new OrderResource($order);
     }
     public function store(StoreOrderRequest $request)
     {
-        $clientid = Auth::user()->id;
+        $clientid = Auth::user()->typeable_id;
         $order = Order::Create([
 
-            'status'=> $request->status,
-            'is_insured'=> $request->is_insured,
-            'ordered_by_id'=> $clientid,
-            'status'=>'new',
+            'status' => $request->status,
+            'is_insured' => $request->is_insured,
+            'ordered_by_id' => $clientid,
+            'status' => 'new',
 
- 
+
         ]);
         if ($request->hasFile('prescription')) {
 
@@ -42,47 +43,47 @@ class OrderController extends Controller
 
             foreach ($files as $file) {
 
-                $path = $file->store('order-'.$order->id , ['disk'=>'prescription']);
+                $path = $file->store('order-' . $order->id, ['disk' => 'prescription']);
 
                 Prescription::Create([
-                    'order_id'=> $order->id ,
-                    'path'=> $path,
+                    'order_id' => $order->id,
+                    'path' => $path,
                 ]);
             }
         }
         return new OrderResource($order);
     }
-    public function update(StoreOrderRequest $request , Order $order)
+    public function update(StoreOrderRequest $request, Order $order)
     {
         if ($order->status == 'New') {
 
-            if ($request->hasFile('prescription')){
+            if ($request->hasFile('prescription')) {
 
-                $directory = 'order-'.$order->id;
+                $directory = 'order-' . $order->id;
                 Storage::disk('prescription')->deleteDirectory($directory);
 
                 $files = $request->file('prescription');
 
                 foreach ($files as $file) {
 
-                    $path = $file->store('order-'.$order->id, ['disk'=>'prescription']);
+                    $path = $file->store('order-' . $order->id, ['disk' => 'prescription']);
 
                     $order->prescription()->delete();
 
                     Prescription::Create([
-                        'order_id'=> $order->id ,
-                        'path'=> $path,
+                        'order_id' => $order->id,
+                        'path' => $path,
                     ]);
                 }
             }
 
             $order->update([
-                'status'=> $request->status,
+                'status' => $request->status,
             ]);
 
             return new OrderResource($order);
         }
 
-        return response()->json(['message'=>"your order is ".$order->status." you cant change it"] , 406);
+        return response()->json(['message' => "your order is " . $order->status . " you cant change it"], 406);
     }
 }
